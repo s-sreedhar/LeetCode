ab12515bb134a90c3a19f23d5e33f65fe62c1108
Sreedhar Goud <s.sreedhargoud@gmail.com>
2024-02-03 06:15:29 +0530

    Added 0463

---
+++ Solutions/0463/0463.ts
@@ -0,0 +1,16 @@
+function islandPerimeter(grid: number[][]): number {
+    let islands = 0;
+  let neighbors = 0;
+
+  for (let i = 0; i < grid.length; ++i) {
+    for (let j = 0; j < grid[0].length; ++j) {
+      if (grid[i][j] === 1) {
+        ++islands;
+        if (i - 1 >= 0 && grid[i - 1][j] === 1) ++neighbors;
+        if (j - 1 >= 0 && grid[i][j - 1] === 1) ++neighbors;
+      }
+    }
+  }
+
+  return islands * 4 - neighbors * 2;
+};
